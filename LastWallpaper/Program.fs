8b669053fb3ddb5530b97277f27204fdcf92ee3c
Nikolai Voronin <nvoronin@outlook.com>
2023-06-04 21:15:47 +0300

    Add app draft

---
+++ LastWallpaper/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+open WinFormFs
+open System.Windows.Forms
+open System.Drawing
+
+[<Literal>]
+let AppName = "The Last Wallpaper"
+[<Literal>]
+let AppVersion = "3.6.4"
+[<Literal>]
+let GitHubProjectUrl = "https://github.com/nikvoronin/LastWallpaper"
+
+let mainNotifyIcon =
+    SystemTray.createIcon SystemIcons.Application // TODO: STUB: replace with proper icon
+    |> SystemTray.setContextMenu
+        ( Menu.createContext
+            [ "&Update Now" |> Menu.stub__TODO
+            ; "&Open Wallpapers Folder" |> Menu.stub__TODO
+            ; Menu.separator ()
+            ; $"&About {AppName} {AppVersion}"
+                |> Menu.verb
+                    (fun _ ->
+                        Sys.openUrlInBrowser
+                            GitHubProjectUrl
+                    )
+            ; Menu.separator ()
+            ; "&Quit" |> Menu.verb App.exitA
+            ]
+        )
+    |> SystemTray.showIcon
+
+[<EntryPoint; STAThread>]
+let main argv =
+    use _ = mainNotifyIcon
+    App.run () |> ignore
+    0
