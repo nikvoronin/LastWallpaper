553177594ef2b8e5aceaa57f3cf2a0a15cdd5a8e
Nikolai Voronin <nvoronin@outlook.com>
2024-06-16 00:25:56 +0300

    Add user agent header to http client

--- LastWallpaper/Program.cs
+++ LastWallpaper/Program.cs
@@ -20,6 +20,9 @@ internal static class Program
             new WindowsFormsSynchronizationContext() );
 
         var client = new HttpClient();
+        client.DefaultRequestHeaders.Add(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36" );
 
         var notifyIconCtrl =
             new NotifyIcon() {
