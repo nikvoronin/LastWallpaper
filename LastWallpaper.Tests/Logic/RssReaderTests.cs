bce3c74a18bff17435c7829bd3eddf769a58e39f
Nikolai Voronin <nvoronin@outlook.com>
2024-08-06 21:38:35 +0300

    Add unit tests for RssReader

---
+++ LastWallpaper.Tests/Logic/RssReaderTests.cs
@@ -0,0 +1,47 @@
+using FluentAssertions;
+using LastWallpaper.Logic;
+using Moq;
+using Moq.Protected;
+using System.Net;
+
+namespace LastWallpaper.Tests.Logic;
+
+public class RssReaderTests
+{
+    public const string _xmlFileName = "./Models/Rss/elementy.xml";
+
+    [Fact]
+    public async void CanDownloadAndParseFeed()
+    {
+        // Arrange
+        const string url = "url://abc.com";
+
+        var httpMessageHandler = new Mock<HttpMessageHandler>( MockBehavior.Strict );
+
+        httpMessageHandler
+            .Protected()
+            .Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.Is<HttpRequestMessage>( x =>
+                    x.Method == HttpMethod.Get
+                    && x.RequestUri!.ToString().Contains( url ) ),
+                ItExpr.IsAny<CancellationToken>() )
+            .ReturnsAsync(
+                new HttpResponseMessage {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StreamContent( File.OpenRead( _xmlFileName ) )
+                } );
+
+        var httpClient = new HttpClient( httpMessageHandler.Object );
+        var rssReader = new RssReader( httpClient );
+
+        // Act
+        var actual = await rssReader.ParseFeedAsync( url, CancellationToken.None );
+
+        // Assert
+        actual.Should().NotBeNull();
+        actual.Value.Should().NotBeNull();
+        actual.Value.Channel.Should().NotBeNull();
+        actual.Value.Channel.Items.Should().NotBeNullOrEmpty();
+    }
+}
---
+++ LastWallpaper.Tests/Models/Rss/RssFeedTests.cs
@@ -0,0 +1,57 @@
+using FluentAssertions;
+using LastWallpaper.Models.Rss;
+using System.Xml.Serialization;
+
+namespace LastWallpaper.Tests.Models.Rss;
+
+public class RssFeedTests
+{
+    public const string _xmlFileName = "./Models/Rss/elementy.xml";
+
+    [Fact]
+    public void CanBeDeserialized()
+    {
+        // Arrange
+        var serializer = new XmlSerializer( typeof( RssFeed ) );
+        using var stream = File.OpenRead( _xmlFileName );
+
+        // Act
+        var actual = serializer.Deserialize( stream );
+
+        // Assert
+        actual.Should().NotBeNull();
+        actual.Should().BeOfType<RssFeed>();
+    }
+
+    [Fact]
+    public void CanReflectRssXml()
+    {
+        // Arrange
+        var serializer = new XmlSerializer( typeof( RssFeed ) );
+        using var stream = File.OpenRead( _xmlFileName );
+
+        // Act
+        var actual = (RssFeed)serializer.Deserialize( stream )!;
+
+        // Assert
+        actual.Should().NotBeNull();
+
+        actual.Channel.Should().NotBeNull();
+        actual.Channel.Items.Should().NotBeNullOrEmpty();
+
+        var item = actual.Channel.Items[0];
+
+        item.Title
+            .Should().NotBeNullOrWhiteSpace();
+
+        item.PubDate.Year
+            .Should().BeGreaterThan( 2000 );
+
+        item.Enclosure
+            .Should().NotBeNull();
+        item.Enclosure.Type
+            .Should().NotBeNullOrEmpty();
+        item.Enclosure.Url
+            .Should().NotBeNullOrEmpty();
+    }
+}
